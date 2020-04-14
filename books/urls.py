d939442994c7b3764880d8bf2d24bb8a0a08e619
Andre Williams <awjrs22@gmail.com>
2020-04-14 10:45:47 -0700

    fixes models

--- books/urls.py
+++ books/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home),
     #book/<int:book_id>
-    path('<int:book_id>', views.detail, name='detail')
+    path('<int:book_id>/', views.detail, name='detail')
 ]
